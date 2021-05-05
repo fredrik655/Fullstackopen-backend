df63176a786b2b3bee4facf1604e5ba955de6be4
anon <anon@example.com>
2021-05-05 15:08:33 +0200

    Part 4 done.

--- part3.1-3.6/index.js
+++ part3.1-3.6/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 
-const persons = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-12345",
@@ -49,6 +49,13 @@ app.get('/api/persons/:id',(request, response) => {
   else {
     response.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
