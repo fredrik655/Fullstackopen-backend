082cfa02330e7b55be87befa5980ff63f39137d9
Fredrik655 <26602147+fredrik655@users.noreply.github.com>
2021-09-25 09:26:54 +0200

    exercise 3.1 done and som other stuff

--- part3/index.js
+++ part3/index.js
@@ -4,108 +4,81 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
-app.use(express.json());
-morgan.token('body', function (req) { return JSON.stringify(req.body) })
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens.body(req),
-  ].join(' ')
-}));
-
-app.use(cors());
+app.use(express.json()); // middleware that looks at http header and if it is of type json it parses it and attach it to the req body
 
 
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-12345",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
-    id: 4
-  },
-  {
-    name: "test",
-    number: "39-23-6423122",
-    id: 4
+let notes = [  
+  {    
+    id: 1,    
+    content: "HTML is easy",    
+    date: "2019-05-30T17:30:31.098Z",    
+    important: true  },
+  {    
+    id: 2,   
+    content: "Browser can execute only Javascript",    
+    date: "2019-05-30T18:39:34.091Z",    
+    important: false  },  
+  {    
+    id: 3,    
+    content: "GET and POST are the most important methods of HTTP protocol",    
+    date: "2019-05-30T19:20:14.298Z",    
+    important: true  
   }
 ];
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
-});
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.get('/info', (request, response) => {
-  response.send(`
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  `);
-});
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+})
 
-app.get('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id); // or Number(req.params.id)
+  const note = notes.find(note => note.id === id);
+  console.log(note);
 
-  if(person) {
-    response.json(person);
+  if(note) {
+    res.json(note);
   }
   else {
-    response.status(404).end();
+    res.status(404).end();
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  console.log(notes);
+  notes = notes.filter(note => note.id !== id);
+  console.log(notes);
 
-  response.status(204).end();
+  res.status(204).end();
 });
 
-const generateRandomId = () => {
-  return Math.floor(Math.random() * 1000);
-}
-
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  if(!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing"
-    });
-  }
-  else if (persons.find(person => person.name === body.name)){
-    return response.status(400).json({
-      error: 'name must be unique'
-    });
-  }
+app.post('/api/notes', (req, res) => {
   
-  const newPerson = {
-    name: body.name,
-    number: body.number,
-    id: generateRandomId()
+  const body = req.body;
+
+  if(!body.content) {
+    return res.status(400).json({
+      error: 'content missing'
+    });
   }
-  persons = persons.concat(newPerson);
-  response.json(newPerson);
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note);
+  res.json(note);
 });
 
-const PORT = process.env.PORT | 3001;
+const PORT =  3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
